6171cc99c63495f7b22645df72d724cccd0f5f47
pipixia626 <48346436+pipixia626@users.noreply.github.com>
2021-04-20 18:47:48 +0800

    Create vector.cpp

---
+++ ch3/vector.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+
+
+vector<int> vl(10); // vl 有 10 个元素，每个的值都是 0
+vector<int> v2{10};// v2 有 1 个元素，该元素的值是 10
+vector<int> v3(10, 1); // v3 有 10 个元素，每个的值都是 1
+vector<int> v4{10, 1};// v4 有 2 个元素，值分别是 10 和 1
+
+}
+
+
+
